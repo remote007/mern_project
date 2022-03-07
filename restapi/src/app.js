56771cd4ab2bfab08134128438b2022a38769193
remote007 <pmnrai2013@gmail.com>
2022-03-07 07:05:38 +0530

    update

--- restapi/src/app.js
+++ restapi/src/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-app.get("/students",(req,res) => {
-    res.send("Hell! from the other side");
+const port = process.env.PORT || 8000;
+// app.get("/",(req,res) => {
+//     res.send("Hello! from the other side by me , I'm healthy");
+// })
+app.post("/student",(req,res)=>{
+    res.send("Hello! from the other side");
 })
-app.listen();
+app.listen(port , ()=>{
+     console.log(`Connection Established at ${port}`);
+});
