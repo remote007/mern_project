a3a38b7b780cc9b66f6c11f8d9b1fa1601169172
remote007 <pmnrai2013@gmail.com>
2022-03-07 10:39:30 +0530

    Update app.js

--- restapi/src/app.js
+++ restapi/src/app.js
@@ -3,10 +3,13 @@ const app = express();
 require("./db/conn");
 const Student = require("./models/students");
 const port = process.env.PORT || 8000;
+app.use(express.json())
+
 // app.get("/",(req,res) => {
 //     res.send("Hello! from the other side by me , I'm healthy");
 // })
 app.post("/student",(req,res)=>{
+    console.log(req.body);
     res.send("Hello! from the other side");
 })
 app.listen(port , ()=>{
