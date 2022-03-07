1078bb8421dceafd0d611956bd7c81abe3874607
remote007 <pmnrai2013@gmail.com>
2022-03-07 08:02:13 +0530

    Update students.js

--- restapi/src/models/students.js
+++ restapi/src/models/students.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const { default: isEmail } = require("validator/lib/isEmail");
 const studentSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true,
         minlength:3
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:[true,"Email already present !"],
+        validate(value){
+            if(!isEmail(value))
+                throw new Error("Not a valid email");
+        }
+
+    },
+    phone:{
+        type:Number,
+        unique:true,
+        required:true,
+        minlength:10,
+        maxlength:10
+    },
+    address:{
+        type:String,
+        required:true
     }
 });
