5577d14cf26099903d28f9635a3dbdba7dacb106
remote007 <pmnrai2013@gmail.com>
2022-03-09 05:47:36 +0530

    update

--- restapi/src/app.js
+++ restapi/src/app.js
@@ -3,6 +3,8 @@ const app = express();
 require("./db/conn");
 const Student = require("./models/students");
 const port = process.env.PORT || 8000;
+const studentRouter = require("./routers/student")
+app.use(studentRouter)
 app.use(express.json())
 
 // app.get("/",(req,res) => {
@@ -20,80 +22,80 @@ app.use(express.json())
 //     // res.send("Hello! from the other side");
 // })
 // Async Await to get result of promise and avoid then and catch , here error handling is done through try and catch
-app.post("/student",async(req,res)=>{
-    try{
-        const user = new Student(req.body);
-        const createUser = await user.save();
-        res.status(201).send(createUser);
-    }catch(err){
-        res.status(400).send(err);
-    }
+// app.post("/student",async(req,res)=>{
+//     try{
+//         const user = new Student(req.body);
+//         const createUser = await user.save();
+//         res.status(201).send(createUser);
+//     }catch(err){
+//         res.status(400).send(err);
+//     }
 
-})
+// })
 
-app.get("/students", async(req,res)=>{
+// app.get("/students", async(req,res)=>{
 
-    try{
-        const studentsData = await Student.find();
-        console.log(studentsData);
-        res.status(201).send(studentsData);
+//     try{
+//         const studentsData = await Student.find();
+//         console.log(studentsData);
+//         res.status(201).send(studentsData);
 
-    }catch(err){
-        res.status(400).send(err);
-    }
+//     }catch(err){
+//         res.status(400).send(err);
+//     }
 
-})
-// get by student individual id 
-app.get("/student/:id",async(req,res)=>{
-    try{
-        const studentData = await Student.findById(req.params.id);
-        if(!studentData){
-            res.status(400).send("Incorrect Data");
-        }else{
-            res.status(201).send(studentData);
-        }
+// })
+// // get by student individual id 
+// app.get("/student/:id",async(req,res)=>{
+//     try{
+//         const studentData = await Student.findById(req.params.id);
+//         if(!studentData){
+//             res.status(400).send("Incorrect Data");
+//         }else{
+//             res.status(201).send(studentData);
+//         }
         
-    }catch(err){
-        res.status(400).send(err);
-    }
+//     }catch(err){
+//         res.status(400).send(err);
+//     }
 
-})
-//delete the document or row
+// })
+// //delete the document or row
 
-app.delete("/student/:id",async(req,res) => {
+// app.delete("/student/:id",async(req,res) => {
 
-    try{
-        const _id = req.params.id;
-        const deletedUser = await Student.findByIdAndDelete({"_id":_id});
-        if(!deletedUser){
-            res.status(400).send("Id Not Found");
-        }else{
-            res.status(201).send(deletedUser);
-        }
+//     try{
+//         const _id = req.params.id;
+//         const deletedUser = await Student.findByIdAndDelete({"_id":_id});
+//         if(!deletedUser){
+//             res.status(400).send("Id Not Found");
+//         }else{
+//             res.status(201).send(deletedUser);
+//         }
         
-    }catch(err){
-        res.status(400).send(err);
-    }
+//     }catch(err){
+//         res.status(400).send(err);
+//     }
 
-})
-// update student with put and patch
-app.patch("/student/:id",async(req,res)=>{
-    try{
-        const _id = req.params.id;
-        const updatedUser = await Student.findByIdAndUpdate({"_id":_id},req.body,{new:true});
-        if(!updatedUser){
-            res.status(400).send("Invalid Id");
-        }else{
+// })
+// // update student with put and patch
+// app.patch("/student/:id",async(req,res)=>{
+//     try{
+//         const _id = req.params.id;
+//         const updatedUser = await Student.findByIdAndUpdate({"_id":_id},req.body,{new:true});
+//         if(!updatedUser){
+//             res.status(400).send("Invalid Id");
+//         }else{
 
-            res.status(201).send(updatedUser);
-        }
+//             res.status(201).send(updatedUser);
+//         }
 
-    }catch(err){
-        res.status(400).send(err);
-    }
+//     }catch(err){
+//         res.status(400).send(err);
+//     }
 
 
-})
+// })
 
 
 // listening to the server should always be at the bottom of application 
---
+++ restapi/src/routers/student.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const Student = require("../models/students");
+const router = new express.Router();
+
+
+router.post("/students",async(req,res)=>{
+    try{
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
+
+})
+
+router.get("/students", async(req,res)=>{
+
+    try{
+        const studentsData = await Student.find();
+        console.log(studentsData);
+        res.status(201).send(studentsData);
+
+    }catch(err){
+        res.status(400).send(err);
+    }
+
+})
+// get by student individual id 
+router.get("/student/:id",async(req,res)=>{
+    try{
+        const studentData = await Student.findById(req.params.id);
+        if(!studentData){
+            res.status(400).send("Incorrect Data");
+        }else{
+            res.status(201).send(studentData);
+        }
+        
+    }catch(err){
+        res.status(400).send(err);
+    }
+
+})
+//delete the document or row
+
+router.delete("/student/:id",async(req,res) => {
+
+    try{
+        const _id = req.params.id;
+        const deletedUser = await Student.findByIdAndDelete({"_id":_id});
+        if(!deletedUser){
+            res.status(400).send("Id Not Found");
+        }else{
+            res.status(201).send(deletedUser);
+        }
+        
+    }catch(err){
+        res.status(400).send(err);
+    }
+
+})
+// update student with put and patch
+router.patch("/student/:id",async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const updatedUser = await Student.findByIdAndUpdate({"_id":_id},req.body,{new:true});
+        if(!updatedUser){
+            res.status(400).send("Invalid Id");
+        }else{
+
+            res.status(201).send(updatedUser);
+        }
+
+    }catch(err){
+        res.status(400).send(err);
+    }
+
+
+})
+
+module.exports = router;
