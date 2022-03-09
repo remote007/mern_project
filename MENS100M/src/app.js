f0514f69eaa6daa1b93a5d169e251c19906bd6cb
remote007 <pmnrai2013@gmail.com>
2022-03-09 10:55:02 +0530

    update

--- MENS100M/src/app.js
+++ MENS100M/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express();
+require("./db/conn")
 // process's environment will give port if to host remote online otherwise 3000 for loalhost
 const port = process.env.PORT || 8000;
 
--- MENS100M/src/db/conn.js
+++ MENS100M/src/db/conn.js
@@ -1,5 +1,7 @@
-const mongoose = requestAnimationFrame("mongoose")
+const mongoose = require("mongoose")
 // it returns promise so using then and catch , arrow function is called call back function
 mongoose.connect("mongodb://localhost:27017/olympics").then(()=>{
         console.log("MongoDB connection established sucessfully");
+}).catch((err)=>{
+    console.log("No connection  ",err);
 })
