6bf02cc565d9a5163e6e80017e1d3838c098a4ab
remote007 <pmnrai2013@gmail.com>
2022-03-09 10:14:52 +0530

    Update app.js

--- MENS100M/src/app.js
+++ MENS100M/src/app.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const app = express();
 // process's environment will give port if to host remote online otherwise 3000 for loalhost
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
+
+app.get("/hi",async(req,res) => {
+
+    res.send("!");
+
+})
+
+
+
 // listen application 
-app.listen(()=>{
-    console.log("Connection established successfully ");
+app.listen(port,()=>{
+    console.log(`Connection established successfully at port ${port}`);
 })
