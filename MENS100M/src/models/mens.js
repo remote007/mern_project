3739fba3b8ad2afbab958f8d302592fb94d77b51
remote007 <pmnrai2013@gmail.com>
2022-03-09 11:39:20 +0530

    Update mens.js

--- MENS100M/src/models/mens.js
+++ MENS100M/src/models/mens.js
@@ -2,5 +2,25 @@ const express = require("express")
 const mongoose = require("mongoose")
 
 const menSchema = new mongoose.Schema({
-    
+    ranking:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    dob:{
+        type:Date,
+        required:true,
+        trim:true
+    },
+    country:{
+        type:String,
+        required:true,
+        trim:true
+    }
+
 });
