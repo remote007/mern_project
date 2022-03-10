7ba3b565d1843a231e8947e0facde3761140a266
remote007 <pmnrai2013@gmail.com>
2022-03-10 06:46:13 +0530

    Update a.js

--- MENS100M/src/routers/a.js
+++ MENS100M/src/routers/a.js
@@ -5,4 +5,12 @@ router.use(express.json())
 
 router.post("/mens",async(req,res)=>{
 
+    try{
+        const newMan = new mens(req.body);
+        const man = await newMan.save();
+        res.status(201).send(man);
+
+    }catch(err){
+       res.status(400).send(err);
+    }
 })
