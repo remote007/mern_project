3d6c1cf0e5174bafd250313d41d5b3d5d4d3dc0d
remote007 <pmnrai2013@gmail.com>
2022-03-10 10:41:55 +0530

    update

--- MENS100M/src/app.js
+++ MENS100M/src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 require("./db/conn")
 require("./models/mens")
-const mensRouter = require("./routers/a")
+const mensRouter = require("./routers/mensRouter")
 // process's environment will give port if to host remote online otherwise 3000 for loalhost
 const port = process.env.PORT || 8000;
 app.use(mensRouter);
