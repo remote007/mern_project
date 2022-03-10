4fe7e8b78928dc2ed0259279be2997564b62e34b
remote007 <pmnrai2013@gmail.com>
2022-03-10 19:39:13 +0530

    Update app.js

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -10,10 +10,13 @@ const path = require("path");
 // gives complete directory path from the drive to src and put "../public" , to go back as public folder is in mern project
 const static_path = path.join(__dirname,"../public");
 app.use(express.static(static_path))
-
+// setting views engine
+app.set("view engine","hbs")
 //  `/`hi is the root and req and res are objects 
 app.get("/hi",async(req,res)=>{
-    res.send("Hi , I love and then develop then master habits. I built the world I live in")
+    // res.send("Hi , I love and then develop then master habits. I built the world I live in")
+    // rendering dynamic html file to respond
+    res.render("index")
 })
 app.listen(port,()=>{
     console.log(`Server runs successfully on ${port}`)
