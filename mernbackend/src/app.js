2a2b186ab48471fb7806efdb0c1dbb3c52a8b4ec
remote007 <pmnrai2013@gmail.com>
2022-03-10 18:43:55 +0530

    Update app.js

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -6,10 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("./db/conn")
 const path = require("path");
-// gives complete directory path from the drive and put "../public" , if on terminal inside src to go back as public folder is in mern project
-const static_path = path.join(__dirname,"/public");
+// gives complete directory path from the drive to src and put "../public" , to go back as public folder is in mern project
+const static_path = path.join(__dirname,"../public");
 app.use(express.static(static_path))
-
 
 //  `/`hi is the root and req and res are objects 
 app.get("/hi",async(req,res)=>{
