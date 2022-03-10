dcd57bccf4fd5544540c24c5bb6ed3b6fbd83bd9
remote007 <pmnrai2013@gmail.com>
2022-03-10 13:43:11 +0530

    Update app.js

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express();
 const port = process.env.PORT || 3000;
+app.get("/hi",async(req,res)=>{
+    res.send("Hi , I love and then develop then master habits. I built the world I live in")
+})
 app.listen(port,()=>{
     console.log(`Server runs successfully on ${port}`)
 })
