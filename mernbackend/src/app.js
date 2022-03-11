a220ff8922e846abb7a4d73ac37dd697a0be8e0f
remote007 <pmnrai2013@gmail.com>
2022-03-11 07:25:38 +0530

    Update app.js

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -9,6 +9,10 @@ require("./db/conn")
 const path = require("path");
 // gives complete directory path from the drive to src and put "../public" , to go back as public folder is in mern project
 const static_path = path.join(__dirname,"../public");
+// when using views folder inside src directly no need of providing path
+const template_path = path.join(__dirname,"../templates/views")
+// tell express earlier views is now here
+app.set("views",template_path)
 app.use(express.static(static_path))
 // setting views engine
 app.set("view engine","hbs")
