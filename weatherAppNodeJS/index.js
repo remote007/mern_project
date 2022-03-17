51614f326fd594d0cc5b13f0f924e783993dab7d
remote007 <pmnrai2013@gmail.com>
2022-03-17 08:10:49 +0530

    Update index.js

--- weatherAppNodeJS/index.js
+++ weatherAppNodeJS/index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const fs = require("fs");
+const requests = require("requests");
 const homeFile = fs.writeFileSync("home.html" ,"utf-8");
 const server = http.createServer((req,res)=>{
     if(req.url=="/"){
         //installed with npm and now require it
-        const requests = require("requests");
+       
     }
 });
