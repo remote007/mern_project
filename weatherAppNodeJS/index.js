fac9f98fff274062ec46dd91fb0ccbfac3db0db4
remote007 <pmnrai2013@gmail.com>
2022-03-17 10:27:45 +0530

    update

--- weatherAppNodeJS/index.js
+++ weatherAppNodeJS/index.js
@@ -3,14 +3,22 @@ const fs = require("fs");
 'use strict';
 const requests = require("requests");
 const homeFile = fs.readFileSync("home.html" ,"utf-8");
+
+const replaceVal = (homeFile,val) =>{
+    
+}
 const server = http.createServer((req,res)=>{
     // req is request from internal server;
     if(req.url=="/"){
         //installed with npm and now require it , requests is used to send it from the application
        requests("https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=95444cb615c878ae792a1033de5525b9")
        .on('data', function (chunk) {
-        const onjdata = JSON.parse(chunk);
-        console.log(onjdata)
+        const objdata = JSON.parse(chunk);
+        const arr = [objdata];
+        // console.log(arr[0].main.temp);
+        const realTimeData = arrData.map((val)=>{
+            replaceVal(homeFile,val);
+        })
       })
       .on('end', function (err) {
         if (err) return console.log('connection closed due to errors', err);
