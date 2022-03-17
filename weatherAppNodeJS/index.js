2668de4e1e56b7bea6566b611116075f805d70c1
remote007 <pmnrai2013@gmail.com>
2022-03-17 08:56:44 +0530

    Update index.js

--- weatherAppNodeJS/index.js
+++ weatherAppNodeJS/index.js
@@ -1,14 +1,15 @@
 const http = require("http");
 const fs = require("fs");
 const requests = require("requests");
-const homeFile = fs.writeFileSync("home.html" ,"utf-8");
+const homeFile = fs.readFileSync("home.html" ,"utf-8");
 const server = http.createServer((req,res)=>{
     // req is request from internal server;
     if(req.url=="/"){
         //installed with npm and now require it , requests is used to send it from the application
-       requests("api.openweathermap.org/data/2.5/weather?q=Pune&appid=95444cb615c878ae792a1033de5525b9")
-       .on(data,(chunk)=>{
-            console.log()
+       requests("api.openweathermap.org/data/2.5/weather?q=Pune&appid=95444cb615c878ae792a1033de5525b9",
+       {streaming})
+       .on("data",(chunk)=>{
+            console.log(chunk);
        })
        .on("end",(err)=>{
         if(err){
