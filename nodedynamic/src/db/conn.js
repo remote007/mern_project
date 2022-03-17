a152debeac6d9bbf1fedbdd3b9e4ed9362d03d29
remote007 <pmnrai2013@gmail.com>
2022-03-17 17:02:51 +0530

    Update conn.js

--- nodedynamic/src/db/conn.js
+++ nodedynamic/src/db/conn.js
@@ -1,2 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/nodeDynamic");
+mongoose.connect("mongodb://localhost:27017/nodeDynamic")
+.then(()=>{
+    console.log("MongoDB Connected Successfully !");
+});
