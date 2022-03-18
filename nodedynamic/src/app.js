1cfe568f91f9fc3b86f1408d4cd86396bdbfb67d
remote007 <pmnrai2013@gmail.com>
2022-03-18 19:40:12 +0530

    Update app.js

--- nodedynamic/src/app.js
+++ nodedynamic/src/app.js
@@ -6,8 +6,8 @@ require("./db/conn")
 
 const static_path = path.join(__dirname,"../public");
 app.use("/css",express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css/bootstrap-grid.min.css")))
-app.use("/js",express.static(path.join(__dirname,"../node_modules/jquery/dist")))
-app.use(express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")))
+app.use("/jq",express.static(path.join(__dirname,"../node_modules/jquery/dist")))
+app.use("/js",express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")))
 app.use(express.static(static_path))
 
 app.get("/",(req,res)=>{
