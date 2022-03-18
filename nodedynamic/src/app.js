a657683acd3dee5691c05b99545e19a53f6b46f3
remote007 <pmnrai2013@gmail.com>
2022-03-18 08:59:03 +0530

    Update app.js

--- nodedynamic/src/app.js
+++ nodedynamic/src/app.js
@@ -5,7 +5,7 @@ const path = require("path");
 require("./db/conn")
 
 const static_path = path.join(__dirname,"../public");
-
+app.use(express.static(static_path))
 
 app.get("/",(req,res)=>{
     res.send("Hi")
